25d2b91f77ce9ddeae3675782a3f4223aa03e1db
Bill Chen <zhechuan.chen@kuka.com>
2020-10-23 16:08:46 +0800

    Fixed the trap_kill issue.

--- netping.sh
+++ netping.sh
@@ -11,30 +11,38 @@ usage="$0 <hosts-file> <ping-command> <action-when-failed> \n
 note: please use \"\" to quote the command setup
 "
 
-if [[ -n $4 ]]; then
+if [[ $# -ne 3 ]]; then
     echo -e $usage
     exit 1
 fi
+program_name=$0
 
 action4failed_pid=0
-
 function is_action4failed_running()
 {
     if [[ $action4failed_pid -eq 0 ]]; then
-        # not start
-        echo "action4failed did not started ..."
+        # not started
+        #echo "action4failed did not started ..."
         return 0
     else
         if [[ -z $(ps -ef | awk '{print $2}' | grep $action4failed_pid) ]]; then
             # started but finished
             action4failed_pid=0
-            echo "action4failed is stopped..."
+            #echo "action4failed is stopped..."
             return 0
         else    # started and ongoing
-            echo "action4failed is running..."
+            #echo "action4failed is running..."
             return 1
         fi
     fi
+}
+
+function trap_kill()
+{
+    pids="$@"
+    for p in $pids; do
+        kill $p
+    done
 }
 
 function main()
@@ -43,21 +51,24 @@ function main()
     local pingcommand=$2
     local action4failed=$3
 
-    #echo "hostfile: <$hostfile>"
-    #echo "pingcommand: <$pingcommand>"
-    #echo "action4failed: <$action4failed>"
-
-    while [[ 1 ]]; do
-        cat $hostfile | parallel --timeout 1 -j4 $pingcommand
+    while [ 1 ]; do
+        echo "--------------- $(date +'%Y-%m-%d %H:%M:%S.%N') --------------"
+        if [[ $action4failed_pid -ne 0 ]]; then
+            trap "trap_kill $action4failed_pid $(pgrep -f $program_name)" SIGTERM SIGQUIT SIGINT SIGKILL
+        else
+            trap "trap_kill $(pgrep -f $program_name)" SIGTERM SIGQUIT SIGINT SIGKILL
+        fi
+        cat $hostfile | parallel --timeout 1 --colsep ' ' -j4 $pingcommand
         if [[ $? -ne 0 ]]; then
             is_action4failed_running
-            if [[ $? -eq 0 ]]; then
+            if [[ $? -eq 0 && -n $action4failed ]]; then
                 bash -c "$action4failed" &
                 action4failed_pid=$!
-                trap "kill $action4failed_pid" SIGTERM SIGQUIT SIGKILL SIGINT
+                trap "trap_kill $action4failed_pid $(pgrep -f $program_name)" SIGTERM SIGQUIT SIGINT SIGKILL
             fi
         fi
         sleep 1
+        echo "------------------------------------------------------------"
     done
 }
 
